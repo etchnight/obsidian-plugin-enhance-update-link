d14dbcf9a9ffb15a2f5e6b0da8a9a8aede234db6
dualwind <dualwind@qq.com>
2025-07-11 13:36:59 +0800

    feat: 实现自动更新章节标题移动后的wiki链接
    添加功能监控Markdown文件中章节标题的移动和修改，当检测到标题移动时会自动更新所有指向原章节标题的wiki链接。主要修改包括：
    - 监听文件修改事件并提取新旧标题
    - 比较标题变化识别移动操作
    - 自动更新所有相关文件中的wiki链接
    - 显示修改数量的通知

--- main.ts
+++ main.ts
@@ -1,134 +1,197 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Menu,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	MetadataCache,
+	MarkdownFileInfo,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Heading {
+	heading: string;
+	level: number;
+	position: number;
+	file: TFile;
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+	metadataCache: MetadataCache;
+	modifiedFiles: { oldFile: TFile | null; newFile: TFile | null } = {
+		oldFile: null,
+		newFile: null,
+	};
+	//removedHeadings对应oldFile，addedHeadings对应newFile
+	movedHeadings: { removedHeadings: Heading[]; addedHeadings: Heading[] } = {
+		removedHeadings: [],
+		addedHeadings: [],
+	};
 	async onload() {
-		await this.loadSettings();
+		this.metadataCache = this.app.metadataCache;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// Listen for file changes
+		this.registerEvent(
+			this.app.vault.on("modify", async (file: TFile) => {
+				if (file.extension === "md") {
+					await this.handleFileModification(file);
 				}
+			})
+		);
+	}
+
+	onunload() {}
+
+	async handleFileModification(file: TFile) {
+		const fileContent = await this.app.vault.read(file);
+		const newHeadings = this.extractHeadings(fileContent, file);
+		const oldHeadings = (
+			this.metadataCache.getFileCache(file)?.headings || []
+		).map((heading) => {
+			return {
+				heading: heading.heading,
+				level: heading.level,
+				position: heading.position.start.line,
+				file: file,
+			};
+		});
+		// Compare old and new headings to find moved ones
+		const addedHeadings = this.findChangedHeadings(
+			oldHeadings,
+			newHeadings
+		);
+		const removedHeadings = this.findChangedHeadings(
+			newHeadings,
+			oldHeadings
+		);
+		if (addedHeadings.length > 0) {
+			this.modifiedFiles.newFile = file;
+			this.movedHeadings.addedHeadings = addedHeadings;
+		} else if (removedHeadings.length > 0) {
+			this.modifiedFiles.oldFile = file;
+			this.movedHeadings.removedHeadings = removedHeadings;
+		}
+		//*均有表示有标题移动且已完成
+		if (this.modifiedFiles.newFile && this.modifiedFiles.oldFile) {
+			const movedHeadings = this.findMovedHeadings();
+			//console.log({ movedHeadings });
+			if (movedHeadings.length > 0) {
+				await this.updateWikiLinks(movedHeadings);
+				//* 移动完成后清空
+				this.modifiedFiles.newFile = null;
+				this.modifiedFiles.oldFile = null;
+				this.movedHeadings.addedHeadings = [];
+				this.movedHeadings.removedHeadings = [];
+			}
+		}
+	}
+
+	/**
+	 * 提取标题
+	 * @param content
+	 * @returns
+	 */
+	extractHeadings(content: string, file: TFile): Heading[] {
+		const headings: Heading[] = [];
+		const lines = content.split("\n");
+		const headingRegex = /^(#{1,6})\s+(.*)$/;
+
+		lines.forEach((line, index) => {
+			const match = line.match(headingRegex);
+			if (match) {
+				headings.push({
+					heading: match[2],
+					level: match[1].length,
+					position: index,
+					file: file,
+				});
 			}
 		});
+		return headings;
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+	/**
+	 * 找到从oldHeadings没有，在newHeadings中有的标题
+	 * 这仅表示标题的增加或删除，移动并没有完成
+	 * @param oldHeadings
+	 * @param newHeadings
+	 * @returns
+	 */
+	findChangedHeadings(
+		oldHeadings: Heading[],
+		newHeadings: Heading[]
+	): Heading[] {
+		const movedHeadings: Heading[] = [];
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		newHeadings.forEach((newHeading) => {
+			const existHeading = oldHeadings.find(
+				(h) => h.heading === newHeading.heading
+			);
+			if (!existHeading) {
+				movedHeadings.push(newHeading);
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		return movedHeadings;
 	}
 
-	onunload() {
-
+	/**
+	 * 找到已移动的标题
+	 * @returns newFile仅作为调试备用
+	 */
+	findMovedHeadings(): (Heading & { newFile: TFile })[] {
+		const movedHeadings: (Heading & { newFile: TFile })[] = [];
+		for (const addedHeading of this.movedHeadings.addedHeadings) {
+			const removedHeading = this.movedHeadings.removedHeadings.find(
+				(h) => h.heading === addedHeading.heading
+			);
+			if (removedHeading) {
+				movedHeadings.push({
+					...removedHeading,
+					newFile: addedHeading.file,
+				});
+			}
+		}
+		return movedHeadings;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async updateWikiLinks(movedHeadings: (Heading & { newFile: TFile })[]) {
+		let count = 0;
+		const allFiles = this.app.vault.getMarkdownFiles();
+		for (const targetFile of allFiles) {
+			//* 本文件内的引用也要修改
+			//if (targetFile.path === file.path) continue;
+			const content = await this.app.vault.read(targetFile);
+			let newContent = content;
+
+			for (const heading of movedHeadings) {
+				const linkPattern = new RegExp(
+					`\\[\\[${
+						(this.modifiedFiles.oldFile as TFile).basename
+					}#${this.escapeRegExp(heading.heading)}\\]\\]`,
+					"g"
+				);
+				if (!linkPattern.test(content)) continue;
+				newContent = newContent.replace(
+					linkPattern,
+					`[[${(this.modifiedFiles.newFile as TFile).basename}#${
+						heading.heading
+					}]]`
+				);
+				count++;
+			}
+
+			if (newContent !== content) {
+				await this.app.vault.modify(targetFile, newContent);
+			}
+		}
+		new Notice(`已修改${count}个文件中的wiki链接`);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	escapeRegExp(string: string): string {
+		return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 	}
 }
